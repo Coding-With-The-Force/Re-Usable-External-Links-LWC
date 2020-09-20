89f77927dbaf8c6746b5fbca836c0762f25a7a52
Coding With The Force <codingwiththeforce@gmail.com>
2020-09-19 20:45:55 -0500

    Add files via upload
    Added comments to the components

--- External_Links_Wrapper/External_Links_WrapperController.js
+++ External_Links_Wrapper/External_Links_WrapperController.js
@@ -2,11 +2,16 @@
  * Created by gerry on 9/18/2020.
  */
 
+/*Aura component javascript controller*/
+
 ({
+	//This function is utilized to open a new console tab within your console apps
 	openConsoleTab : function(component, event, helper)
 	{
+		//This finds the workspace for our current console app
 		let workspace = component.find("workspace");
 
+		//This opens the url as a tab in the console and auto focuses on that tab
 		workspace.openTab(
 			{
 				url: event.getParam("urlToPass"),
